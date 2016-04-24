66200b5a690e601306a18b74cba36881a86792c4
theJian <thejianmail@gmail.com>
2016-04-24 15:52:42 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     loaders: [
     {
       test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
+      exclude: /node_modules/,
       loader: 'babel',
       query: {
         presets: ['es2015','react']
