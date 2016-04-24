5a581944e9d546e3518a975e578a991bfdf0b3db
theJian <thejianmail@gmail.com>
2016-04-24 16:49:42 +0800

    newsItem domain

--- app/NewsItem.js
+++ app/NewsItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import URL from 'url';
 import './NewsItem.css';
 
 export default class NewsItem extends React.Component {
+  getDomain() {
+    return URL.parse(this.props.item.url).hostname;
+  }
+
   render() {
     return (
         <div className="newsItem">
           <a className="newsItem-titleLink" href={this.props.item.url}>{this.props.item.title}</a>
+          <span className="newsItem-domain">
+            ({this.getDomain()})
+          </span>
         </div>
         );
   }
 }
+
